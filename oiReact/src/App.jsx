0b2566c1515edd9355e189a0b3c5cc917a15beae
caiolorramvalente <caio_valente@estudante.sesisenai.org.br>
2024-08-23 17:16:20 -0300

    adiçao de um projeto em React

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+       <h1>dinossauros🐱‍🐉</h1>
+       <h1>🥩carnívoros🥩</h1>
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint repellat deserunt, quis dignissimos doloremque omnis, 
+        veniam architecto earum saepe quas, accusamus quos dolorem hic neque vitae. Aut provident sed itaque?</p>
+      
+    </>
+  )
+}
+
+export default App
